8a675c1e0c85155c18bc4d43e348a582e569bc5b
coraljiao <mrjiaohs@163.com>
2019-04-14 21:22:20 +0800

    每日算法

--- src/main/java/com/xjtu/ssmidea/jianzhioffer/day08.java
+++ src/main/java/com/xjtu/ssmidea/jianzhioffer/day08.java
@@ -8,7 +8,7 @@ package com.xjtu.ssmidea.jianzhioffer;
 public class day08 {
     public static void main(String[] args) {
         int[] arr = {5, 3, 8, 2, 9};
-        sort081(arr);
+        sort01(arr);
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
@@ -35,5 +35,29 @@ public class day08 {
         arr[i] = temp;
         sort082(arr, left, i - 1);
         sort082(arr, i + 1, right);
+    }
+
+    //快排
+    public static void sort01(int[] arrs) {
+        sort02(arrs, 0, arrs.length - 1);
+    }
+
+    private static void sort02(int[] arrs, int left, int right) {
+        if (left > right) return;
+        int i = left, j = arrs.length - 1;
+        int temp = arrs[i];
+        while (i < j) {
+            while (i < j && arrs[j] >= temp) {
+                j--;
+            }
+            arrs[i] = arrs[j];
+            while (i < j && arrs[i] <= temp) {
+                i++;
+            }
+            arrs[j] = arrs[i];
+        }
+        arrs[i] = temp;
+        sort02(arrs, left, i - 1);
+        sort02(arrs, i + 1, right);
     }
 }
