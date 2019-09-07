894bd45759fa12a0572695909154afcd30765e9d
coraljiao <mrjiaohs@163.com>
2019-09-07 16:28:58 +0800

    每日算法

--- src/main/java/com/xjtu/qiuzhao/前中后序遍历.java
+++ src/main/java/com/xjtu/qiuzhao/前中后序遍历.java
@@ -9,7 +9,9 @@ import java.util.Stack;
  */
 public class 前中后序遍历 {
     public static void main(String[] args){
-
+        TreeNode treeNode=new TreeNode();
+        treeNode.left=new TreeNode(2);
+        treeNode.right=new TreeNode(3);
     }
     //实现思路：
     //前序遍历的顺序是：根结点 -> 左孩子 -> 右孩子
@@ -48,6 +50,32 @@ public class 前中后序遍历 {
                     head = head.right;
                 }
             }
+        }
+    }
+    //实现思路：
+    //后序遍历的顺序：左孩子 -> 右孩子 -> 根结点
+    //仔细想一下，打印每个结点需要访问根结点三次，先从根结点开始找到左孩子，返回再找到右孩子，再返回到根结点，需要访问根结点三次，直接按照当前顺序进行遍历不知如何下手，那么我们可以换一个角度去考虑。
+    //栈结构是先进后出的，那我们不妨借助一个栈先存储 根结点 -> 右孩子 -> 左孩子 的结果，再将其依次弹出就是后序遍历的顺序了。
+    //那实现 根结点 -> 右孩子 -> 左孩子 的方法就很简单了，回忆一下刚才说的前序遍历的方式，前序遍历是先要左孩子，就后压入左孩子，反之，将前序遍历的逻辑改写为：弹出每个栈顶结点作为当前结点并存储到一个辅助栈中，如果当前结点有左孩子就先压入左孩子，如果有右孩子就后压入右孩子，每次取栈顶弹出到辅助栈中。最后将得到的辅助栈中元素依次弹出得到的就是后序遍历的结果
+    public static void posOrderTraverse(TreeNode head) {
+        System.out.println("pos-order");
+        if(head != null) {
+            Stack<TreeNode> stack1 = new Stack<>();
+            Stack<TreeNode> stack2 = new Stack<>();     // 辅助栈，存储 根 -> 右 -> 左 的结果
+            stack1.push(head);
+            while(!stack1.isEmpty()) {
+                head = stack1.pop();
+                stack2.push(head);
+                // 有左孩子就先压入左孩子
+                if(head.left != null)
+                    stack1.push(head.left);
+                // 有右孩子就后压入右孩子
+                if(head.right != null)
+                    stack1.push(head.right);
+            }
+            // 逆序打印 根 -> 右 -> 左 的结果，就是后序遍历的结果
+            while(!stack2.isEmpty())
+                System.out.print(stack2.pop().val + " ");
         }
     }
 }
